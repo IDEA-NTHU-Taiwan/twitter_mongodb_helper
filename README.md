beb6f1dd466183bbe6c969a8c22bf60c7786434e
Jherez Taylor <jherez.taylor@gmail.com>
2017-02-26 23:47:39 +0800

    Updated with parallel processing overview

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Twitter MongoDB helper functions
 
-A collection of python functions for dealing with mongoDB indexes, aggregation pipleline and map-reduce
+A collection of python functions for dealing with MongoDB indexes, aggregation pipleline and map-reduce
 [TwitterMongoDBHelper](https://idea-nthu-taiwan.github.io/twitter_mongodb_helper/)
 
 
@@ -21,7 +21,7 @@ It is essential to create indexes on your fields in order to speed of the run ti
 
 This assumes that you already have MongoDB installed and running and already populated with tweets.
 
-Start the mongo shell from the terminal and select your databse
+Start the mongo shell from the terminal and select your database
 
 ```bash
 mongo
@@ -130,3 +130,91 @@ db.tweets.aggregate(
   ]
 )
 ```
+## Parallel Processing
+
+As the operations that you need to do become more complex, at some point you will start feeling the effects
+of waiting on MongoDB to send data across the network or even loading from disk. MongoDB has a `parallCollectionScan` commmand
+but as of this writing it does not work if you are using the WiredTiger Storage Engine. (All new MongoDB installs use this by default).
+A feature request has been opened for the last two years, see [ticket](https://jira.mongodb.org/browse/SERVER-17688). If we want to process a collection
+in parallel and not waste idle CPU time waiting on data to arrive then we have to get creative.
+
+[Joblib](https://pythonhosted.org/joblib/parallel.html) is high level Python library for multiprocessing and threading and it's really easy to convert your
+functions to use it. If you can read a list comprehension then you can use the library, just look at the example in their docs.
+
+The basic idea of the trick I'm proposing is to first get a count of the number of documents in a collection and divide it into `k` slices, `k` being the number
+of sub processes or threads that we want to spawn. We then use `k` connection objects and pass each slice is then passed to some function to be processed.
+This approach should be database agnostic and should even work for raw files. The main idea is to split up the work.
+
+#### Outline
+
+First determine the size of your partitions, `partition_size = sample_size // num_cores`.
+
+`sample_size` is the size of the collection and `num_cores` is the number of processor cores or the number of sub processes to spawn.
+You may need to experiment with this, a general rule of thumb is 1 process for each core your processor has.
+
+We need to set the start and end indices for the collection we want to access in parallel
+Example, with `collection_size = 2001`, `num_cores = 4`, the list comprehension
+below gives the following `skip:limit` indices:
+
+```python
+partitions = [(i, partition_size) for i in range(0, sample_size, partition_size)]
+```
+`(0, 500), (500, 500), (1000, 500), (1500, 500), (2000, 500)`
+
+We then deal with the last index since it exceeds the size of the collection
+```python
+partitions[-1] = (partitions[-1][0], (sample_size - partitions[-1][0]))
+```
+
+The last index then becomes `(2000, 1)`
+
+Finally, we let joblib do its magic. `process_partition` is simply a function
+that creates a connection object for each sub process and
+appends a list with the values from the DB collection.
+
+```python
+results = Parallel(n_jobs=num_cores)(
+      delayed(process_partition)(partition, connection_params) for partition in partitions)
+```
+
+```python
+def process_partition(partition, connection_params):
+    client = connect()
+    db_name = connection_params[0]
+    collection = connection_params[1]
+    dbo = client[db_name]
+
+    cursor = dbo[collection].find({}, {"_id": 1}).skip(
+        partition[0]).limit(partition[1])
+    documents = {str(document["_id"]) for document in cursor}
+    return documents
+```
+
+#### Results
+
+It wouldn't be any good if I talked about all this and didn't provide any proof
+of the speedup we can get. Let's look at some stats. All tests were executed on my
+laptop which has an Intel Core i5-5200u with 2 physical cores and 4 threads. The MongoDB
+has installed locally so there were no network delay issues at play. As mentioned, play with the
+`num_cores` value and do a few tests to determine if your function is [CPU or IO bound.](https://www.quantstart.com/articles/parallelising-python-with-threading-and-multiprocessing)
+Multiprocessing for CPU and Threading for IO. You can set joblib to use threading, see [here.](https://pythonhosted.org/joblib/parallel.html#using-the-threading-backend)
+The tests are available in `run.py`
+
+The following shows the results `process_partition` with 1M and 2M records. Linear is the same logic but all within a loop.
+Best of three.
+
+![collection_read](data/parallel_test.png)
+
+As we can see, the function gains the most speedup from using multiprocessing.
+
+Next up we have a more complex function, here the time gap starts to widen. In the complex function
+I process a list of objects and so I experimented with making that parallel as well. Imagine there are two levels,
+the top level is where I spawn `k` connection objects as usual and at the second level I process a list in parallel.
+It's not possible to spawn a sub process within a sub process, but it is possible to spawn a thread and that combination
+provided the most improvement. The function was run on 100K records.
+
+![complex_function](data/parallel_hs_test.png)
+
+The difference between the parallel combinations might appear to be minimal but as you scale those point values start to matter.
+Just to give you an idea of how this all works out in a real world scenario, I ran a complex function on 6M records. Doing it linearly took
+4 hours, with multiprocessing I got that down to 1 hour. :open_mouth:
--- run.py
+++ run.py
@@ -7,24 +7,36 @@ This module provides methods to query the MongoDB instance
 """
 
 import os
-import json
+import re
+import string
+from time import time
+import cProfile
 import glob
 import csv
 from pprint import pprint
-import collections
+import pstats
+import requests
+from itertools import chain
+from joblib import Parallel, delayed, cpu_count
 import pymongo
+import ujson
 from bson.son import SON
 from bson.code import Code
 from bson.objectid import ObjectId
+from nltk.util import ngrams
+from nltk.corpus import stopwords
 
 JSON_PATH = ""
 CSV_PATH = ""
 DATA_PATH = "data/"
-DB_URL = "mongodb://url:port"
+DB_URL = os.environ["MONGODB_URL"]
 HASHTAGS = "entities.hashtags"
 USER_MENTIONS = "entities.user_mentions"
 HASHTAG_LIMIT = 50
 USER_MENTIONS_LIMIT = 50
+PUNCTUATION = list(string.punctuation)
+STOP_LIST = dict.fromkeys(stopwords.words(
+    "english") + PUNCTUATION + ["rt", "via", "RT"])
 
 
 def timing(func):
@@ -33,9 +45,9 @@ def timing(func):
     def wrap(*args):
         """Wrapper
         """
-        time1 = time.time()
+        time1 = time()
         ret = func(*args)
-        time2 = time.time()
+        time2 = time()
         print '%s function took %0.3f ms' % (func.func_name, (time2 - time1) * 1000.0)
         return ret
     return wrap
@@ -44,6 +56,7 @@ def timing(func):
 def do_cprofile(func):
     """Decorator for profiling a function
     """
+
     def profiled_func(*args, **kwargs):
         """Wrapper
         """
@@ -54,7 +67,9 @@ def do_cprofile(func):
             profile.disable()
             return result
         finally:
-            profile.print_stats(sort='time')
+            stats = pstats.Stats(profile)
+            stats.sort_stats("time").print_stats(20)
+
     return profiled_func
 
 
@@ -72,6 +87,38 @@ def connect():
     return conn
 
 
+def send_job_completion(run_time, args):
+    """Format and print the details of a completed job
+
+    Args:
+        run_time (list): Start and end times.
+        args (list): Contains the following:
+            0: function_name (str): Name of the function that was run.
+            1: message_text  (str): Text to be sent in notification.
+    """
+
+    time_diff = round((run_time[1] - run_time[0]), 2)
+    print "%s function took %0.3f seconds" % (args[0], time_diff)
+    send_notification = send_job_notification(
+        "Lab Computer: " + ": " + args[1] + " took " + str(time_diff) + " seconds", "Complete")
+    print send_notification.content
+
+
+def send_job_notification(title, body):
+    """ Send a notification via Pushbullet.
+
+     Args:
+        json_obj (json_obj).
+
+    Indicates whether a job has completed or whether an error occured.
+    """
+    headers = {"Access-Token": 'PUSHBULLET_API_KEY',
+               "Content-Type": "application/json"}
+    payload = {"type": "note", "title": title, "body": ujson.dumps(body)}
+    url = "https://api.pushbullet.com/v2/pushes"
+    return requests.post(url, headers=headers, data=ujson.dumps(payload))
+
+
 def get_filenames():
     """Reads all the json files in the folder and removes the drive and path and
     extension, only returning a list of strings with the file names.
@@ -82,7 +129,7 @@ def get_filenames():
     file_path = glob.glob(JSON_PATH + "*.json")
     result = []
     for entry in file_path:
-        drive, path = os.path.splitdrive(entry)
+        _, path = os.path.splitdrive(entry)
         path, filename = os.path.split(path)
         name = os.path.splitext(filename)[0]
         result.append(str(name))
@@ -94,15 +141,13 @@ def read_json_file(filename, path):
     Args:
         filename   (str): Filename to be loaded.
         path       (str): Directory path to use.
-
     Returns:
-        list: List of words concatenated with OR.
+        obj: json object
     """
-
     result = []
     try:
-        with open(path + filename + '.json', 'r') as entry:
-            result = json.load(entry)
+        with open(path + filename + ".json", "r") as entry:
+            result = ujson.load(entry)
     except IOError as ex:
         print "I/O error({0}): {1}".format(ex.errno, ex.strerror)
     else:
@@ -112,13 +157,13 @@ def read_json_file(filename, path):
 
 def write_json_file(filename, path, result):
     """Writes the result to json with the given filename.
+
     Args:
         filename   (str): Filename to write to.
         path       (str): Directory path to use.
     """
-
-    with open(path + filename + '.json', 'w+') as json_file:
-        json.dump(result, json_file)
+    with open(path + filename + ".json", "w+") as json_file:
+        ujson.dump(result, json_file)
     json_file.close()
 
 
@@ -196,10 +241,9 @@ def test_file_operations():
     Test previous methods
     """
     file_list = get_filenames()
-    extract_corpus(file_list)
     num_entries = count_entries(file_list)
     pprint(num_entries)
-    res = read_csv_file('about_sexual_orientation_eng_pg1', CSV_PATH)
+    res = read_csv_file('csvname', CSV_PATH)
     res2 = build_query_string(res)
     print res2
 
@@ -295,7 +339,7 @@ def create_lang_subset(client, db_name, lang):
     dbo.tweets.aggregate(pipeline)
 
 
-def get_top_k_users(client, db_name, lang_list, k_filter, limit):
+def get_top_k_users(client, db_name, lang_list, k_filter):
     """Finds the top k users in the collection.
     k_filter is the name of an array in the collection, we apply the $unwind operator to it
 
@@ -304,7 +348,6 @@ def get_top_k_users(client, db_name, lang_list, k_filter, limit):
         db_name     (str):  Name of database to query.
         lang_list   (list): List of languages to match on.
         k_filter    (str):  Name of an array in the collection.abs
-        limit       (int):  Limit for the number of results to return.
 
     Returns:
         list: List of objects containing id_str, screen_name and the frequency of appearance.
@@ -452,6 +495,7 @@ def hashtag_map_reduce(client, db_name, subset, output_name):
     write_json_file('hashtag_distribution_mr', DATA_PATH, frequency)
     pprint(frequency)
 
+
 def find_by_object_id(client, db_name, subset, object_id):
     """Fetches the specified object from the specified collection
 
@@ -465,6 +509,7 @@ def find_by_object_id(client, db_name, subset, object_id):
     cursor = dbo[subset].find({"_id": ObjectId(object_id)})
     pprint(cursor["_id"])
 
+
 def get_hashtag_collection(client, db_name, subset):
     """Fetches the specified hashtag collection and writes it to a json file
 
@@ -476,7 +521,8 @@ def get_hashtag_collection(client, db_name, subset):
     dbo = client[db_name]
     cursor = dbo[subset].find({"count": {"$gt": 500}}, {
                               "hashtag": 1, "count": 1, "_id": 0})
-    fileops.write_json_file(subset, constants.DATA_PATH, list(cursor))
+    write_json_file(subset, DATA_PATH, list(cursor))
+
 
 def finder(client, db_name, subset, k_items):
     """Fetches k obects from the specified collection
@@ -493,17 +539,130 @@ def finder(client, db_name, subset, k_items):
         pprint(document)
         pprint(str(document["_id"]))
 
+
+@do_cprofile
+def create_ngrams(text_list, length):
+    """ Create ngrams of the specified length from a string of text
+    Args:
+        text_list   (list): Pre-tokenized text to process.
+        length      (int):  Length of ngrams to create.
+    """
+
+    clean_tokens = [token for token in text_list if token not in PUNCTUATION]
+    return [" ".join(i for i in ngram) for ngram in ngrams(clean_tokens, length)]
+
+
+def remove_urls(raw_text):
+    """ Removes urls from text
+
+    Args:
+        raw_text (str): Text to filter.
+    """
+    return re.sub(
+        r"(?:http|https):\/\/((?:[\w-]+)(?:\.[\w-]+)+)(?:[\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?", "", raw_text)
+
+
+@timing
+def test_linear_scan(connection_params, sample_size):
+    """Test linear scan
+    Args:
+        connection_params  (list): Contains connection objects and params as follows:
+            0: db_name     (str): Name of database to query.
+            1: collection  (str): Name of collection to use.
+        sample_size (int): Number of documents to retrieve.
+    """
+    client = connect()
+    db_name = connection_params[0]
+    collection = connection_params[1]
+    dbo = client[db_name]
+
+    cursor = dbo[collection].find({}, {"_id": 1}).limit(sample_size)
+    documents = {str(document["_id"]) for document in cursor}
+    pprint(len(documents))
+
+
+def process_partition(partition, connection_params):
+    """ Thread safe process
+    partition stores a tuple with the skip and limit values
+    Args:
+        partition   (tuple): Contains skip and limit values.
+        connection_params  (list): Contains connection objects and params as follows:
+            0: db_name     (str): Name of database to query.
+            1: collection  (str): Name of collection to use.
+    """
+    client = connect()
+    db_name = connection_params[0]
+    collection = connection_params[1]
+    dbo = client[db_name]
+
+    cursor = dbo[collection].find({}, {"_id": 1}).skip(
+        partition[0]).limit(partition[1])
+    documents = {str(document["_id"]) for document in cursor}
+    return documents
+
+
+@timing
+def parallel_test(num_cores, connection_params, sample_size):
+    """Test parallel functionality
+    Args:
+        num_cores (int): Number of processes to use, usually associated with the number of cores
+        your processor has, check this with cpu_count().
+        connection_params  (list): Contains connection objects and params as follows:
+            0: db_name     (str): Name of database to query.
+            1: collection  (str): Name of collection to use.
+        sample_size (int): Number of documents to retrieve.
+    """
+
+    # Here, we divide the collection in k slices.
+    partition_size = sample_size // num_cores
+
+    # We need to set the start and end indices for the collection we want to access in parallel
+    # Ex. collection_size = 2001, num_cores = 4, the list comprehension
+    # below gives the following skip:limit indices:
+    # (0, 500), (500, 500), (1000, 500), (1500, 500), (2000, 500)
+
+    # We then deal with the last index since it exceeds the size of the
+    # collection
+    partitions = [(i, partition_size)
+                  for i in range(0, sample_size, partition_size)]
+
+    # Account for lists that aren't evenly divisible, update the last tuple to
+    # retrieve the remainder of the items
+    partitions[-1] = (partitions[-1][0], (sample_size - partitions[-1][0]))
+
+    # See https://pythonhosted.org/joblib/parallel.html for more details
+    # The library allows us to pass our function `process_partition` arguments and a
+    # list to be processed. Be sure to check out https://pythonhosted.org/joblib/parallel.html#using-the-threading-backend
+    # and experiment to determine if your function gains more speedup from
+    # using mutiprocessing or threads
+
+    results = Parallel(n_jobs=num_cores)(
+        delayed(process_partition)(partition, connection_params) for partition in partitions)
+    results = list(chain.from_iterable(results))
+    pprint(len(results))
+
+
 def main():
     """
     Test functionality
     """
-    client = connect()
+    # client = connect()
     # user_mentions_map_reduce(client, 'twitter', 'subset_gu')
+
     # test_get_language_distribution(client)
     # test_get_language_subset(client)
     # create_lang_subset(client, 'twitter', 'gu')
-    test_get_top_k_users(client, 'twitter', ['ru'], USER_MENTIONS)
-    test_get_top_k_hashtags(client, 'twitter', ['ru'], HASHTAGS, 20)
-    collection_finder(client, 'twitter', 'hashtag_dist_en')
+
+    # test_get_top_k_users(client, 'twitter', ['ru'], USER_MENTIONS)
+    # test_get_top_k_hashtags(client, 'twitter', ['ru'], HASHTAGS, 20)
+    # get_hashtag_collection(client, 'twitter', 'hashtag_dist_en')
+
+    # tweet_text = "hello world how are you"
+    # pprint(([(create_ngrams(tweet_text.split(" "), i)) for i in range(1, 6)]))
+
+    # sample_size = 100000
+    # connection_params = ["twitter", "tweets"]
+    # test_linear_scan(connection_params, sample_size)
+    # parallel_test(cpu_count(), connection_params, sample_size)
 if __name__ == '__main__':
     main()
